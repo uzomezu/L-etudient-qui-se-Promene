35c4ca1c6e781d2d51de0efc0e0742b0c0c9e367
Kevin Mezu <42013409+uzomezu@users.noreply.github.com>
2020-08-07 10:39:40 -0400

    'etudiant'

--- js/indexUI.js
+++ js/indexUI.js
@@ -3,8 +3,7 @@
   const overlay = document.querySelector(".overlay");
   const img = document.getElementById("img");
   const home = document.querySelector("#home");
-  const bodyDoc = document.getElementById("body");
-  const card = document.querySelector(".card");
+  const aLinks = document.querySelectorAll("a");
   const projects = document.getElementById("projects");
   const arrowSymbol = document.querySelector(".fa-arrow-down");
   const overLayLinks = document.querySelectorAll(".overlay-Link");
@@ -12,6 +11,20 @@
   let opacity = 0;
   let menuOpen = false;
   let projListItems = projects.getElementsByTagName("li");
+  //SFX for links and clicks
+  console.log(projects.querySelectorAll("a"));
+  aLinks.forEach(function (link) {
+    link.addEventListener("mouseover", () => {
+      if (!link.classList.contains("social")) {
+        let snd = new Audio("../sounds/pop-click.wav");
+        snd.play();
+      }
+    });
+    link.addEventListener("click", () => {
+      let audio = new Audio("../sounds/button-confirm-spacey.wav");
+      audio.play();
+    });
+  });
   //Open Menu Hamburger to X
   menuBtn.addEventListener("click", function () {
     if (!menuOpen) {
@@ -39,19 +52,10 @@
       });
     });
   });
-  let rect = projects.getBoundingClientRect();
-
-  const topProj = rect.top;
-  const bottomProj = rect.bottom;
-  const y_Start = innerHeight;
   function animate() {
     requestAnimationFrame(animate);
   }
-  sessionStorage.setItem("top", topProj);
-  sessionStorage.setItem("bottom", bottomProj);
-  console.log(topProj, bottomProj);
   window.addEventListener("scroll", function () {
-    animate();
     if (scrollY < innerHeight) {
       opacity = scrollY / (innerHeight / 3);
     } else {
@@ -64,20 +68,4 @@
     arrowSymbol.style.opacity = `${1 - opacity}`;
     home.style.opacity = `${1 - opacity}`;
   });
-  // Dark/Light UI
-  // const lights = document.getElementById("lights");
-  // const body = document.getElementById("body");
-  // //*** NOTE TO SELF": Do something here so the UI stays the same after the page has been reloaded
-  // lights.addEventListener("click", function () {
-  //   if (body.classList.contains("light-mode")) {
-  //     body.classList.add("dark-mode");
-  //     body.classList.remove("light-mode");
-  //     img.src = "KM_Logo_black.png";
-  //   } else {
-  //     body.classList.add("light-mode");
-  //     body.classList.remove("dark-mode");
-  //     img.src = "KM_Logo_white.png";
-  //   }
-  // });
-  // //
 })();
